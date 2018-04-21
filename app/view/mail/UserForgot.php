6fcfb6b1a5b8f649b42fbb23513ed36617dd6751
吳姿萱 <wu-tzu-hsuan@wuzixuandeMacBook-Air.local>
2018-04-21 23:37:04 +0800

    test

--- app/view/mail/UserForgot.php
+++
@@ -1,4 +0,0 @@
-<div style='display:inline-block;width:100%;height:28px;line-height:28px;'>親愛的 <b><?php echo $user->name;?></b>，您好：</div>
-<div style='display:inline-block;width:100%;height:28px;line-height:28px;'>此為 <b>AD-POST</b> 忘記密碼信件</div>
-<div style='display:inline-block;width:100%;height:28px;line-height:28px;'>請點擊以下連結， 重設您的密碼：</div>
-<div style='display:inline-block;width:100%;height:28px;line-height:28px;'><a href='<?php echo URL::base ('login/reset?id=' . $user->id . '&code=' . $active->token);?>' target='_blank' style='color: rgba(58, 122, 243, 1.00)'><?php echo URL::base ('login/reset?id=' . $user->id . '&code=' . $active->token);?></a></div>
