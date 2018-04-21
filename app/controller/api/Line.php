ddd1f22b505b111da07e9b7ca8624bc7b4f65ca4
吳姿萱 <wu-tzu-hsuan@wuzixuandeMacBook-Air.local>
2018-04-22 01:38:43 +0800

    test

--- app/controller/api/Line.php
+++ app/controller/api/Line.php
@@ -13,17 +13,22 @@ class Line extends ApiController {
   }
 
   public function index() {
+    Log::info(1);
     $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(config('line', 'channelToken'));
+    Log::info(2);
     $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => config('line', 'channelSecret')]);
-    if( !isset ($_SERVER["HTTP_" . HTTPHeader::LINE_SIGNATURE]) ) {
+    Log::info(3);
+    if( !isset ($_SERVER["HTTP_" . HTTPHeader::LINE_SIGNATURE]) )
       return false;
-    }
+    Log::info(4);
     $body = file_get_contents ("php://input");
+    Log::info(5);
     $events = $bot->parseEventRequest ($body, $_SERVER["HTTP_" . HTTPHeader::LINE_SIGNATURE]);
+    Log::info(6);
     foreach( $events as $event ) {
       $response = $bot->replyText( $event->getReplyToken(), 'hello!' );
     }
-
+    Log::info(7);
 
   }
 }
