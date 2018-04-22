872b2e19f5ba8f9c2af3d06653ed2bdd7e028207
吳姿萱 <wu-tzu-hsuan@wuzixuandeMacBook-Air.local>
2018-04-22 17:24:47 +0800

    test

--- app/controller/api/Line.php
+++ app/controller/api/Line.php
@@ -25,7 +25,7 @@ class Line extends ApiController {
     foreach( $events as $event ) {
       if ( $event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage ) {
         Log::info(1);
-        $type = strtolower(trim( $event->getMessage()->type ));
+        $type = strtolower(trim( $event->getMessage() ));
         Log::info(2);
         Log::info($type);
         Log::info($event->getText());
