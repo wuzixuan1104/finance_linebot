8681f2a4474886eefa48bcbdec784a92e4efa556
吳姿萱 <wu-tzu-hsuan@wuzixuandeMacBook-Air.local>
2018-04-22 18:43:59 +0800

    sourec

--- app/controller/api/Line.php
+++ app/controller/api/Line.php
@@ -25,7 +25,7 @@ class Line extends ApiController {
     foreach( $events as $event ) {
       if ( $event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage ) {
         Log::info(1);
-        // Log::info($event->message->type);
+        Log::info($event->getSource());
         $type = strtolower(trim( $event->getType() ));
         Log::info(2);
         Log::info($type);
@@ -82,43 +82,6 @@ class Line extends ApiController {
     //   $result = curl_exec($ch);
     //   curl_close($ch);
     // }
-  // public function index() {
-  //   $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(config('line', 'channelToken'));
-  //   $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => config('line', 'channelSecret')]);
-  //   if( !isset ($_SERVER["HTTP_" . LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE]) )
-  //     return false;
-  //
-  // 	$this->receive = json_decode(file_get_contents("php://input"));
-  // 	$text = $this->receive->events[0]->message->text;
-  // 	$type = $this->receive->events[0]->source->type;
-  //
-  // 	// 由於新版的Messaging Api可以讓Bot帳號加入多人聊天和群組當中
-  // 	// 所以在這裡先判斷訊息的來源
-  // 	if ($type == "room")
-  // 	{
-  // 		// 多人聊天 讀取房間id
-  // 		$this->from = $this->receive->events[0]->source->roomId;
-  // 	}
-  // 	else if ($type == "group")
-  // 	{
-  // 		// 群組 讀取群組id
-  // 		$this->from = $this->receive->events[0]->source->groupId;
-  // 	}
-  // 	else
-  // 	{
-  // 		// 一對一聊天 讀取使用者id
-  // 		$this->from = $this->receive->events[0]->source->userId;
-  // 	}
-  //
-  // 	// 讀取訊息的型態 [Text, Image, Video, Audio, Location, Sticker]
-  // 	$content_type = $this->receive->events[0]->message->type;
-  //
-  // 	// 準備Post回Line伺服器的資料
-  // 	$this->header = ["Content-Type: application/json", "Authorization: Bearer {" . config('line', 'channelToken') . "}"];
-  //   Log::info($this->from);
-  // 	// 回覆訊息
-  // 	$this->reply($content_type, $text);
-  // }
 
 
 }
