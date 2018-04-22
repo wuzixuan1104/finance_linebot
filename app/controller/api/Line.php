605ca036b93f817d1158d1948c012df533e25206
吳姿萱 <wu-tzu-hsuan@wuzixuandeMacBook-Air.local>
2018-04-22 21:03:24 +0800

    test

--- app/controller/api/Line.php
+++ app/controller/api/Line.php
@@ -24,22 +24,32 @@ class Line extends ApiController {
     $msg = new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder();
 
     foreach( $events as $event ) {
-      switch($event->getMessageType()) {
-        case "text":
-          $outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText());
-          $msg->add($outputText);
-          break;
-        case "image":
-          $url = 'https://example.com/image_preview.jpg';
-          $outputText = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($url, $url);
-          break;
-      }
-      $response = $bot->replyMessage($event->getReplyToken(), $msg);
-      // if( $response->isSucceeded() ) {
-      //   echo 'Succeeded';
-      //   return;
+      // switch($event->getMessageType()) {
+      //   case "text":
+      //     $outputText = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText());
+      //     $msg->add($outputText);
+      //     break;
+      //   case "image":
+      //     $url = 'https://example.com/image_preview.jpg';
+      //     $outputText = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($url, $url);
+      //     break;
       // }
-      // echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+      $actions = array(
+        //一般訊息型 action
+        new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder("按鈕1","文字1"),
+        //網址型 action
+        new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("Google","http://www.google.com"),
+        //下列兩筆均為互動型action
+        new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("下一頁", "page=3"),
+        new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("上一頁", "page=1")
+      );
+
+      $img_url = "圖片網址，必需為 https (圖片非必填欄位)";
+      $button = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder("按鈕文字","說明", $img_url, $actions);
+      $msg = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("這訊息要用手機的賴才看的到哦", $button);
+      $bot->replyMessage($replyToken,$msg);
+      // $response = $bot->replyMessage($event->getReplyToken(), $msg);
+
     }
 
   }
