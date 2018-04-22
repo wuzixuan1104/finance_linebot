8830391c9a5102289d1789a2fd2523867ac719a8
吳姿萱 <wu-tzu-hsuan@wuzixuandeMacBook-Air.local>
2018-04-22 16:35:25 +0800

    test

--- app/controller/api/Line.php
+++ app/controller/api/Line.php
@@ -141,6 +141,6 @@ class Line extends ApiController {
 		$context = stream_context_create(array(
 		"http" => array("method" => "POST", "header" => implode(PHP_EOL, $this->header), "content" => json_encode($data), "ignore_errors" => true)
 		));
-		file_get_contents($url, false, $context);
+		echo file_get_contents($url, false, $context);
 	}
 }
