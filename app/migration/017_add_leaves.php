f7b43b829aeeba2b4623c7a8dd4f8b019c4f6719
Ubuntu <ubuntu@ip-172-31-9-129.ap-northeast-1.compute.internal>
2018-04-23 02:47:44 +0000

    create migration

---
+++ app/migration/017_add_leaves.php
@@ -0,0 +1,14 @@
+<?php defined ('OACI') || exit ('此檔案不允許讀取。');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2018, OACI
+ * @license     http://opensource.org/licenses/MIT  MIT License
+ * @link        https://www.ioa.tw/
+ */
+
+return array (
+    'up' => "",
+    'down' => "",
+    'at' => "2018-04-23 10:32:38",
+  );
