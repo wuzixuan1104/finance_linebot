9d374d2a1eef869b751723bd1a2826fc67e89b3a
吳姿萱 <cherry51120@gmail.com>
2018-04-23 14:33:20 +0800

    test

--- app/migration/011_add_files.php
+++ app/migration/011_add_files.php
@@ -14,7 +14,7 @@ return array (
       `mid` int(11) unsigned NOT NULL COMMENT '回覆的訊息id',
       `filename` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '檔案名稱',
       `filesize` int(11) unsigned NOT NULL DEFAULT '' COMMENT '檔案大小',
-      `file` int(11) unsigned NOT NULL DEFAULT '' COMMENT '檔案',
+      `file` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '檔案',
       `reply_token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '回覆token',
       `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新時間',
