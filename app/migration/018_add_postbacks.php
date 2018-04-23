082decfc8e46ff6b88f09aa891d9916e02116cb1
吳姿萱 <cherry51120@gmail.com>
2018-04-23 13:57:31 +0800

    postbacks update

---
+++ app/migration/018_add_postbacks.php
@@ -0,0 +1,25 @@
+<?php defined ('OACI') || exit ('此檔案不允許讀取。');
+
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2013 - 2018, OACI
+ * @license     http://opensource.org/licenses/MIT  MIT License
+ * @link        https://www.ioa.tw/
+ */
+
+return array (
+    'up' => "CREATE TABLE `postbacks` (
+      `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+      `user_id` int(11) unsigned NOT NULL,
+      `data` int(11) unsigned NOT NULL DEFAULT '' COMMENT '資料',
+      `param` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '參數',
+      `reply_token` varchar(191) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT '回覆token',
+      `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新時間',
+      `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增時間',
+      PRIMARY KEY (`id`)
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;",
+
+    'down' => "DROP TABLE `postbacks`;",
+    'at' => "2018-04-23 13:49:02",
+  );
