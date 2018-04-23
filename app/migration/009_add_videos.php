5a1be92f9ebfc6b4efe2f04c7e320f026ca83c40
吳姿萱 <cherry51120@gmail.com>
2018-04-23 14:10:15 +0800

    test

--- app/migration/009_add_videos.php
+++ app/migration/009_add_videos.php
@@ -13,7 +13,7 @@ return array (
     `user_id` int(11) unsigned NOT NULL,
     `mid` int(11) unsigned NOT NULL COMMENT '回覆的訊息id',
     `reply_token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '回覆token',
-    `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `video` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 影片',
     `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新時間',
     `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增時間',
