ed0e2b6304b048750cef378325a8cfe66bb97106
吳姿萱 <cherry51120@gmail.com>
2018-04-23 14:31:34 +0800

    test

--- app/migration/009_add_videos.php
+++ app/migration/009_add_videos.php
@@ -11,10 +11,10 @@ return array (
   'up' => "CREATE TABLE `videos` (
     `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
     `user_id` int(11) unsigned NOT NULL,
-    `mid` int(11) unsigned NOT NULL COMMENT '回覆的訊息id',
+    `mid` int(11) unsigned NOT NULL COMMENT '回覆的訊息ID',
     `reply_token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '回覆token',
-    `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `video` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT 影片',
+    `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    `video` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '影片',
     `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新時間',
     `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '新增時間',
     PRIMARY KEY (`id`)
