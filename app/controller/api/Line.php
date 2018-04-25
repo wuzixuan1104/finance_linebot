bb60ac824383b5740a91d4a09dc2d7069bce38f0
吳姿萱 <cherry51120@gmail.com>
2018-04-25 16:27:44 +0800

    test

--- app/controller/api/Line.php
+++ app/controller/api/Line.php
@@ -75,7 +75,10 @@ class Line extends ApiController {
         MyLineBotMsg::create()->templateCarousel( [
           MyLineBotMsg::create()->templateCarouselColumn('標題', '哈哈哈哈哈', 'https://cdn.adpost.com.tw/adpost/production/uploads/adv_details/pic/00/00/00/00/00/00/06/5e/_29753e27ceb64b0f35b77aca7acf9a3e.jpg', [
             MyLineBotActionMsg::create()->datetimePicker('date', date('Y-m-d'), 'date', '', '', ''),
-            // new \LINE\LINEBot\TemplateActionBuilder\DatetimePickerTemplateActionBuilder('date', date('Y-m-d'), 'datetime', '', '', ''),
+            MyLineBotActionMsg::create()->message('label', 'test'),
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'test'),
+            MyLineBotActionMsg::create()->uri('url', 'http://google.com.tw'),
+
           ])
         ])
       )
