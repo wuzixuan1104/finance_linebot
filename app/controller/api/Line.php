007bfc0b2133b48660c03172b52b0aee19864777
吳姿萱 <wu-tzu-hsuan@wuzixuandeMacBook-Air.local>
2018-05-27 16:14:15 +0800

    test

--- app/controller/api/Line.php
+++ app/controller/api/Line.php
@@ -36,6 +36,7 @@ class Line extends ApiController {
         case 'image':
           Log::info('===========123242');
           $url = $log->file->url();
+      
           Log::info($url);
           MyLineBotMsg::create()
             ->image($url, $url)
