327a0af53a7db957c74b7aa4cc138e48733a1251
吳姿萱 <cherry51120@gmail.com>
2018-05-31 11:14:23 +0800

    test

--- app/lib/MyLineBot.php
+++ app/lib/MyLineBot.php
@@ -173,6 +173,7 @@ class MyLineBotLog {
   private function locationMessage() {
     Log::info('helloo');
     Log::info($this->event->getTitle());
+    Log::info('=================');
     $param = array_merge( $this->getParam(), array('title' => $this->event->getTitle(), 'address' => $this->event->getAddress(), 'latitude' => $this->event->getLatitude(), 'longitude' => $this->event->getLongitude()) );
     Log::info(json_encode($param));
 
