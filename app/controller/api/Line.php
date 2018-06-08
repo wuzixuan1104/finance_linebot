37db187f632f701ebad241b844a260cfc9421ad5
吳姿萱 <cherry51120@gmail.com>
2018-06-08 17:11:00 +0800

    test

--- app/controller/api/Line.php
+++ app/controller/api/Line.php
@@ -101,14 +101,14 @@ class Line extends ApiController {
           MyLineBotMsg::create()->templateCarouselColumn('標題', '哈哈哈哈哈', 'https://cdn.adpost.com.tw/adpost/production/uploads/adv_details/pic/00/00/00/00/00/00/06/5e/_29753e27ceb64b0f35b77aca7acf9a3e.jpg', [
             MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
             MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
-            // MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
             // MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
             // MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
           ]),
           MyLineBotMsg::create()->templateCarouselColumn('標題', '哈哈哈哈哈', 'https://cdn.adpost.com.tw/adpost/production/uploads/adv_details/pic/00/00/00/00/00/00/06/5e/_29753e27ceb64b0f35b77aca7acf9a3e.jpg', [
             MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
             MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
-            // MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
             // MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
             // MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
           ]),
