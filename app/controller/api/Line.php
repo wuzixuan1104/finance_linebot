ff7018a5904dd794efad129cd0885ee6263fe1d6
吳姿萱 <cherry51120@gmail.com>
2018-06-08 16:16:19 +0800

    test

--- app/controller/api/Line.php
+++ app/controller/api/Line.php
@@ -82,20 +82,39 @@ class Line extends ApiController {
   }
 
   public function initIntro($event) {
-    if( !$banks = Bank::find('all', array('where' => array('enable' => Bank::ENABLE_ON ) ) ) )
-      return false;
+    // if( !$banks = Bank::find('all', array('where' => array('enable' => Bank::ENABLE_ON ) ) ) )
+    //   return false;
+    //
+    // $actionArr = [];
+    // $cnt = 0;
+    // foreach( $banks as $bank ) {
+    //   $cnt++;
+    //   $actionMsg = MyLineBotActionMsg::create()->postback($bank->name, 'bank_id=' . $bank->id, $bank->name);
+    //   $actionArr[] = $actionMsg;
+    //   break;
+    //   if($cnt == 5)
+    //     break;
+    // }
 
-    $actionArr = [];
-    $cnt = 0;
-    foreach( $banks as $bank ) {
-      $cnt++;
-      $actionMsg = MyLineBotActionMsg::create()->postback($bank->name, 'bank_id=' . $bank->id, $bank->name);
-      $actionArr[] = $actionMsg;
-      break;
-      if($cnt == 5)
-        break;
-    }
-
+    MyLineBotMsg::create()->template('這訊息要用手機的賴才看的到哦',
+        MyLineBotMsg::create()->templateCarousel( [
+          MyLineBotMsg::create()->templateCarouselColumn('標題', '哈哈哈哈哈', 'https://cdn.adpost.com.tw/adpost/production/uploads/adv_details/pic/00/00/00/00/00/00/06/5e/_29753e27ceb64b0f35b77aca7acf9a3e.jpg', [
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
+          ]),
+          MyLineBotMsg::create()->templateCarouselColumn('標題', '哈哈哈哈哈', 'https://cdn.adpost.com.tw/adpost/production/uploads/adv_details/pic/00/00/00/00/00/00/06/5e/_29753e27ceb64b0f35b77aca7acf9a3e.jpg', [
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
+            MyLineBotActionMsg::create()->postback('label', 'postback', 'postback'),
+          ]),
+        ])
+    )->reply ($event->getReplyToken());
+    die;
     MyLineBotMsg::create ()
       ->multi ([
        MyLineBotMsg::create ()->text ('歡迎使用理財小精靈: )'),
