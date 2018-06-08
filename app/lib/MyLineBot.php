df4526cbfa2c775f552d1870b79e08ac0115c831
吳姿萱 <cherry51120@gmail.com>
2018-06-08 15:26:32 +0800

    test

--- app/lib/MyLineBot.php
+++ app/lib/MyLineBot.php
@@ -335,6 +335,7 @@ class MyLineBotActionMsg {
     return is_string($label) && (isHttp($url) || isHttps($url)) ? new UriTemplateActionBuilder($label, $url) : null;
   }
   public function postback($label, $data, $text) {
+    Log::info('postback');
     return is_string($label) && ($data = is_array($data) ? json_encode($data) : $data ) && !is_null($text) ? new PostbackTemplateActionBuilder($label, $data, $text) : null;
   }
 
