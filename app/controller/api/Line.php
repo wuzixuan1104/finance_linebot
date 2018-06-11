6049a72a1ab8fa81f7c65d770774bc3d90439f4c
吳姿萱 <cherry51120@gmail.com>
2018-06-11 13:48:19 +0800

    test

--- app/controller/api/Line.php
+++ app/controller/api/Line.php
@@ -105,15 +105,8 @@ class Line extends ApiController {
       foreach( $currency as $vcurrency )
         $actionArr[] = MyLineBotActionMsg::create()->postback( $vcurrency->name, array('lib' => 'BankProcess', 'method' => 'searchBank', 'param' => array('currency_id' => $vcurrency->id) ), $vcurrency->name);
       $columnArr[] = MyLineBotMsg::create()->templateCarouselColumn('請選擇貨幣', '查詢外匯', 'https://cdn.adpost.com.tw/adpost/production/uploads/adv_details/pic/00/00/00/00/00/00/06/5e/_29753e27ceb64b0f35b77aca7acf9a3e.jpg', $actionArr);
-      break;
     }
-    // $builder = MyLineBotMsg::create()->template('這訊息要用手機的賴才看的到哦',
-    // MyLineBotMsg::create()->templateCarousel( $columnArr ))->reply ($event->getReplyToken());
-    //
-    //
-    // print_R($builder);
-    // die;
-    // $builder =
+
     MyLineBotMsg::create ()
       ->multi ([
         MyLineBotMsg::create ()->text ('歡迎使用理財小精靈: )'),
@@ -122,9 +115,7 @@ class Line extends ApiController {
           MyLineBotMsg::create()->templateCarousel( $columnArr )
         )
     ])->reply ($event->getReplyToken());
-    // print_r($builder);
-    // ->reply ($event->getReplyToksen());
-    Log::info('end');
+
     die;
   }
 
