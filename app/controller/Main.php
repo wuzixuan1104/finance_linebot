fe634a6bfa9c43ef604d077b121b5cfa13b60cd1
吳姿萱 <cherry51120@gmail.com>
2018-06-12 14:41:12 +0800

    test

--- app/controller/Main.php
+++ app/controller/Main.php
@@ -14,10 +14,11 @@ class Main extends SiteController {
 
   }
   public function index () {
-    $this->layout
-         ->with ('current_url', URL::base (''));
-
-    return $this->view->setPath ('site/index.php');
+    // $this->layout
+    //      ->with ('current_url', URL::base (''));
+    //
+    // return $this->view->setPath ('site/index.php');
+    PassbookRecord::find ('all');
   }
   public function admin () {
     return refresh (Url::base ('admin/login'));
