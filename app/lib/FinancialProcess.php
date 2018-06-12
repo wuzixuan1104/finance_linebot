fac8975ea893b5446b8950b13ab39c69bc0aa6ae
吳姿萱 <cherry51120@gmail.com>
2018-06-12 16:14:45 +0800

    test

--- app/lib/FinancialProcess.php
+++ app/lib/FinancialProcess.php
@@ -37,6 +37,18 @@ class FinancialProcess {
   }
 
   public static function getData($params) {
-    Log::info( '====> getData: ' . json_encode($params) );
+    if( !isset($params['currency_id']) || empty($params['currency_id']) || !isset($params['bank_id']) || empty($params['bank_id']) )
+      return false;
+
+    $msg = '';
+    $conditions = array('where' => array('bank_id = ? and currency_id = ?', $params['bank_id'], $params['currency_id']), 'order' => 'created_at desc', 'limit' => 1 );
+    if( $passbooks = PassbookRecord::find('one', $conditions) )
+      $msg .= "牌告匯率：\r\n => 賣出：" . $passbooks->sell . "\r\n => 買入：" . $passbooks->buy . "\r\n================\r\n";
+
+    if( $cashes = CashRecord::find('one', $conditions) )
+      $msg .= "現鈔匯率：\r\n => 賣出：" . $cashes->sell . "\r\n => 買入：" . $cashes->buy;
+
+    return MyLineBotMsg::create()
+              ->text($msg);
   }
 }
