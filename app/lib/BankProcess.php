0c1c03d025b92b1a0616eb79dfcac06ddff00505
吳姿萱 <cherry51120@gmail.com>
2018-06-12 14:14:58 +0800

    test

--- app/lib/BankProcess.php
+++ app/lib/BankProcess.php
@@ -14,11 +14,13 @@ class BankProcess {
   }
 
   public static function searchBank($params) {
+    Log::info('bank 1');
     if( !isset($params['currency_id']) || empty($params['currency_id']) )
       return false;
-
+    Log::info('bank 2');
     if( !$records = PassbookRecord::find('all', array( 'where' => array( "( bank_id, currency_id, created_at ) in ( select `bank_id`, `currency_id`, max(`created_at`) from `passbook_records` where `currency_id` = ? group by `bank_id` ) ", $params['currency_id']) )) )
       return false;
+    Log::info('bank 3');
     $columnArr = [];
     $records = array_chunk( $records, 3 );
     foreach( $records as $key => $record ) {
@@ -28,8 +30,8 @@ class BankProcess {
       foreach( $record as $vrecord )
         $actionArr[] = MyLineBotActionMsg::create()->postback( $vrecord->bank->name, array('lib' => 'BankProcess', 'method' => 'searchData', 'param' => array('currency_id' => $params['currency_id'], 'bank_id' => $vrecord->bank->id) ), $vrecord->bank->name);
       $columnArr[] = MyLineBotMsg::create()->templateCarouselColumn('請選擇銀行', '查詢外匯', 'https://cdn.adpost.com.tw/adpost/production/uploads/adv_details/pic/00/00/00/00/00/00/06/5e/_29753e27ceb64b0f35b77aca7acf9a3e.jpg', $actionArr);
-      break;
     }
+
     return MyLineBotMsg::create()->template('這訊息要用手機的賴才看的到哦',
       MyLineBotMsg::create()->templateCarousel( $columnArr )
     );
