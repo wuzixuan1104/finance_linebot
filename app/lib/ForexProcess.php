4d774c0c45f3eb5ab29200e99f71d78c75a5f61b
吳姿萱 <cherry51120@gmail.com>
2018-06-20 15:40:49 +0800

    test

--- app/lib/ForexProcess.php
+++ app/lib/ForexProcess.php
@@ -14,6 +14,9 @@ class ForexProcess {
   public function __construct() {
   }
 
+  /* 目的：開始功能選單
+   * 呼叫此function 的模式 Follow, join, 傳入字串'hello'
+   */
   public static function begin() {
     if( !$currencies = Currency::find('all', array('where' => array('enable' => Currency::ENABLE_ON ) ) ) )
       return false;
@@ -41,7 +44,10 @@ class ForexProcess {
     return MyLineBotMsg::create()->multi ($multiArr);
   }
 
-  public static function getBanks($params, $log) {
+  /* 目的：取得貨幣對應的銀行
+   * 方法：postback回傳呼叫
+   */
+  public static function getBanks($params, $log = '') {
     if( !isset($params['currency_id']) || empty($params['currency_id']) )
       return false;
 
@@ -49,9 +55,6 @@ class ForexProcess {
       return false;
 
     foreach( array_chunk( $records, 3 ) as $key => $record ) {
-      // if($key > 9) break;
-      // if(count($record) != 3) break;
-
       $actionArr = [];
       foreach( $record as $vrecord )
         $actionArr[] = MyLineBotActionMsg::create()->postback( $vrecord->bank->name, array('lib' => 'ForexProcess', 'method' => 'getRecords', 'param' => array('currency_id' => $params['currency_id'], 'bank_id' => $vrecord->bank->id) ), $vrecord->bank->name);
@@ -60,7 +63,6 @@ class ForexProcess {
       if( ($recordSub = 3 - count($record)) != 0 )
         for( $i = 0; $i < $recordSub; $i++ )
           $actionArr[] = MyLineBotActionMsg::create()->postback( '-', array(), '-');
-
       $columnArr[] = MyLineBotMsg::create()->templateCarouselColumn('請選擇銀行', '查詢外匯', null, $actionArr);
     }
 
@@ -71,28 +73,26 @@ class ForexProcess {
     }, array_chunk($columnArr, 10) );
 
     return MyLineBotMsg::create()->multi ($multiArr);
-
-    // return MyLineBotMsg::create()->template('這訊息要用手機的賴才看的到哦',
-    //   MyLineBotMsg::create()->templateCarousel( $columnArr )
-    // );
   }
 
+  /* 目的：顯示選擇的貨幣銀行紀錄
+   * 方法：postback回傳呼叫
+   */
   public static function getRecords($params, $log = '') {
     if( !isset($params['currency_id']) || empty($params['currency_id']) || !isset($params['bank_id']) || empty($params['bank_id']) )
       return false;
-
-    if( !$currency = Currency::find_by_id($params['currency_id']) )
-      return false;
-    if( !$bank = Bank::find_by_id($params['bank_id']) )
+    if( !$currency = Currency::find_by_id($params['currency_id']) || !$bank = Bank::find_by_id($params['bank_id']) )
       return false;
 
     $msg = "[ " . $currency->name . " / " . $bank->name . " ]\r\n\r\n";
     $conditions = array('where' => array('bank_id = ? and currency_id = ?', $params['bank_id'], $params['currency_id']), 'order' => 'created_at desc', 'limit' => 1 );
+
     if( $passbooks = PassbookRecord::find('one', $conditions) ) {
       $msg .= "牌告匯率：\r\n => 賣出：" . $passbooks->sell . "\r\n => 買入：" . $passbooks->buy;
       if( $time = CurrencyTime::find_by_id($passbooks->currency_time_id))
         $msg .= "\r\n\r\n(" . $time->datetime . ")" . "\r\n================\r\n";
     }
+
     if( $cashes = CashRecord::find('one', $conditions) ) {
       $msg .= "現鈔匯率：\r\n => 賣出：" . $cashes->sell . "\r\n => 買入：" . $cashes->buy;
       if( $time = CurrencyTime::find_by_id($passbooks->currency_time_id))
@@ -109,9 +109,12 @@ class ForexProcess {
                     MyLineBotActionMsg::create()->postback( "台幣 -> " . $currency->name, array('lib' => 'ForexProcess', 'method' => 'getCalcType', 'param' => array('type' => 'calcA', 'currency_id' => $params['currency_id'], 'bank_id' => $params['bank_id'], 'passbook_buy' => $passbooks->buy, 'cash_buy' => $cashes->buy, 'name' => $currency->name ) ), '台幣 -> ' . $currency->name),
                     MyLineBotActionMsg::create()->postback( $currency->name . " -> 台幣", array('lib' => 'ForexProcess', 'method' => 'getCalcType', 'param' => array('type' => 'calcB', 'currency_id' => $params['currency_id'], 'bank_id' => $params['bank_id'], 'passbook_buy' => $passbooks->buy, 'cash_buy' => $cashes->buy, 'name' => $currency->name ) ), $currency->name . ' -> 台幣'),
                   ])]
-                ))]);
+            ))]);
   }
 
+  /* 目的：匯率換算
+   * 方法：postback回傳呼叫
+   */
   public static function getCalcType($params, $log) {
     if( !isset($params['type']) || empty($params['type']) || !isset($params['currency_id']) || empty($params['currency_id']) || !isset($params['bank_id']) || empty($params['bank_id']) )
       return false;
@@ -124,8 +127,7 @@ class ForexProcess {
       'data' => $params,
     ));
     if( !$source->save() )
-      Log::info('source insert fail');
-
+      return false;
     return  MyLineBotMsg::create ()->text('請輸入金額(元)');
   }
 }
