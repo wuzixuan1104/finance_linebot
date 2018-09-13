1d9b480c161370abc0d828ee832b8c4f168533bf
吳姿萱 <cherry51120@gmail.com>
2018-09-13 16:06:33 +0800

    fix code

---
+++ app/lib/postback/Richmenu.php
@@ -0,0 +1,50 @@
+<?php defined('MAPLE') || exit('此檔案不允許讀取！');
+
+Load::lib('MyLineBot.php');
+
+class Search {
+
+  public static function create() {
+    if(!$currencies = \M\Currency::all(['where' => ['enable = ?', \M\Currency::ENABLE_ON]]) )
+      return false;
+
+    $currencies = array_chunk($currencies, 5);
+
+    $flexes = [];
+    $bubbles = [];
+    foreach($currencies as $currency) {
+      foreach($currency as $v) {
+        $flexes[] = FlexBox::create([
+                      FlexBox::create([FlexText::create($v->name)])->setLayout('vertical')->setFlex(7),
+                      FlexSeparator::create(),
+                      FlexButton::create('primary')->setColor('#E6AE5F')->setFlex(3)->setHeight('sm')->setGravity('center')->setAction(FlexAction::postback('選擇', null, json_encode(['lib' => 'postback/RichMenu', 'class' => 'Search', 'method' => 'getCurrency', 'param' => ['currencyId' => $v->id]])))
+                    ])->setLayout('horizontal')->setSpacing('md');
+
+        $flexes[] = FlexSeparator::create();
+      }
+
+      $bubbles[] = FlexBubble::create([
+                    'header' => FlexBox::create([FlexText::create('選擇貨幣')->setWeight('bold')->setSize('lg')->setColor('#E9ECEF')])->setSpacing('xs')->setLayout('horizontal'),
+                    'body' => FlexBox::create($flexes)->setLayout('vertical')->setSpacing('md')->setMargin('sm'),
+                    'styles' => FlexStyles::create()->setHeader(FlexBlock::create()->setBackgroundColor('#3A5762'))
+                  ]);
+      $flexes = [];
+    }
+
+    return MyLineBotMsg::create()->flex('貨幣類別', FlexCarousel::create($bubbles)); 
+  }
+
+  public static function getCurrency() {
+
+  }
+}
+
+class Calculate {
+   public static function create() {
+
+  }
+}
+
+class History {
+
+}
