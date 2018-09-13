a2ac964e99c3be53a2f97388e79256cb0a9e111d
吳姿萱 <cherry51120@gmail.com>
2018-09-13 16:58:13 +0800

    fix code

--- app/lib/postback/Richmenu.php
+++ app/lib/postback/Richmenu.php
@@ -26,7 +26,7 @@ class Search {
       $bubbles[] = FlexBubble::create([
                     'header' => FlexBox::create([FlexText::create('選擇貨幣')->setWeight('bold')->setSize('lg')->setColor('#E9ECEF')])->setSpacing('xs')->setLayout('horizontal'),
                     'body' => FlexBox::create($flexes)->setLayout('vertical')->setSpacing('md')->setMargin('sm'),
-                    'styles' => FlexStyles::create()->setHeader(FlexBlock::create()->setBackgroundColor('#344a4c'))
+                    'styles' => FlexStyles::create()->setHeader(FlexBlock::create()->setBackgroundColor('#075365'))
                   ]);
       $flexes = [];
     }
