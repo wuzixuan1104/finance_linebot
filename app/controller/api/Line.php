27235f6394073e808e0637022926e14fd63a1942
吳姿萱 <cherry51120@gmail.com>
2018-09-13 16:05:54 +0800

    fix code

--- app/controller/api/Line.php
+++ app/controller/api/Line.php
@@ -41,19 +41,19 @@ class Line extends Controller {
           $data = json_decode( $log->data, true );
 
 
-          if(isset( $data['lib'], $data['class'], $data['method'] ) ) {
-            Log::info(0);
-            if(( isset( self::$cache['lib'][$data['lib']] ) ? true : ( Load::lib($data['lib'] . '.php') ? self::$cache['lib'][$data['lib']] = true : true ) )) {
-              Log::info('1:' . json_encode(self::$cache['lib']));
-              if(method_exists($class = $data['class'], $method = $data['method'])) {
-                Log::info(2);
-                if($msg = $class::$method( $data['param'], $source )) {
-                  Log::info(3);
-                }
-              }
-            }
-          }
-          die;
+          // if(isset( $data['lib'], $data['class'], $data['method'] ) ) {
+          //   Log::info(0);
+          //   if(( isset( self::$cache['lib'][$data['lib']] ) ? true : ( Load::lib($data['lib'] . '.php') ? self::$cache['lib'][$data['lib']] = true : true ) )) {
+          //     Log::info('1:' . json_encode(self::$cache['lib']));
+          //     if(method_exists($class = $data['class'], $method = $data['method'])) {
+          //       Log::info(2);
+          //       if($msg = $class::$method( $data['param'], $source )) {
+          //         Log::info(3);
+          //       }
+          //     }
+          //   }
+          // }
+          // die;
           if( !( isset( $data['lib'], $data['class'], $data['method'] ) && ( isset( self::$cache['lib'][$data['lib']] ) ? true : ( Load::lib($data['lib'] . '.php') ? self::$cache['lib'][$data['lib']] = true : false ) )
             && method_exists($class = $data['class'], $method = $data['method']) && $msg = $class::$method( $data['param'], $source ) ) )
             {
