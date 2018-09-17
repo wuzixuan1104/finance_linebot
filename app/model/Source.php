27f66ca8f19fba8212fba9bf117ce6e322d13b2f
吳姿萱 <cherry51120@gmail.com>
2018-09-17 17:10:07 +0800

    fix code

--- app/model/Source.php
+++ app/model/Source.php
@@ -19,7 +19,7 @@ class Source extends Model {
 
   public static function getTitle($event) {
     $response = \MyLineBot::bot()->getProfile($event->getUserId());
-    Log::info($response);
+    \Log::info($response);
     
     if ($response->isSucceeded() && $profile = $response->getJSONDecodedBody())
       return $profile['displayName'];
@@ -37,8 +37,8 @@ class Source extends Model {
         return $obj = Source::create($params);
       });
 
-      if(!RichMenuGenerator::create4user($sid))
-        Log::error('建立richmenu失敗:' . $sid);
+      if(!\RichMenuGenerator::create4user($sid))
+        \Log::error('建立richmenu失敗:' . $sid);
     }
     return $obj;
   }
